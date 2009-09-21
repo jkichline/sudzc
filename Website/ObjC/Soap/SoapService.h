e86da53f9f24e77e490cbc68b909f782b65139d0
jkichline <jkichline@gmail.com>
2009-09-21 21:03:57 +0000

    Updated to allow developers to set a default handler for all requests that can be used to handle general onerror or onfault responses.

--- Website/ObjC/Soap/SoapService.h
+++ Website/ObjC/Soap/SoapService.h
@@ -1,16 +1,20 @@
-﻿@interface SoapService : NSObject
+﻿#import "SoapDelegate.h"
+
+@interface SoapService : NSObject
 {
 	NSString* serviceUrl;
 	NSString* namespace;
 	NSDictionary* headers;
 	BOOL logging;
+	id<SoapDelegate> defaultHandler;
 }
 
-	@property (retain) NSString* serviceUrl;
-	@property (retain) NSString* namespace;
-	@property (retain) NSDictionary* headers;
-	@property BOOL logging;
-	
-	- (id) initWithUrl: (NSString*) url;
+@property (retain) NSString* serviceUrl;
+@property (retain) NSString* namespace;
+@property (retain) NSDictionary* headers;
+@property BOOL logging;
+@property (nonatomic, retain) id<SoapDelegate> defaultHandler;
+
+-(id) initWithUrl: (NSString*) url;
 
 @end

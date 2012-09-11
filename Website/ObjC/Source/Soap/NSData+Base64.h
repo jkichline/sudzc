7075cc7783197b5cc2993d11c062ee719ccfd748
Jason Kichline <jkichline@gmail.com>
2012-09-10 22:47:06 -0400

    Delete old files

--- Website/ObjC/Source/Soap/NSData+Base64.h
+++
@@ -1,9 +0,0 @@
-@interface NSData (Base64) 
-
-+ (NSData *)dataWithBase64EncodedString:(NSString *)string;
-- (id)initWithBase64EncodedString:(NSString *)string;
-
-- (NSString *)base64Encoding;
-- (NSString *)base64EncodingWithLineLength:(unsigned int) lineLength;
-
-@end
